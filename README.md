6eaa3010ac259429924cde7f58f114d0cd064c07
Craftint <shajeer@craftinteractive.ae>
2024-06-25 08:40:37 +0400

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Vat 201
+
+UAE VAT 201 report corrected with multicurrency
+
+#### License
+
+MIT
---
+++ vat_201/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Vat 201",
+			"type": "module",
+			"label": _("Vat 201")
+		}
+	]
